9565632d7007f24464d82030bcb7386d81deaf1b
Rohit  Maity <116875489+TheHydrator@users.noreply.github.com>
2024-10-27 15:27:10 -0500

    Add files via upload

---
+++ Community Classroom/Idea_1_Project/Inputs.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.print("please enter some input: ");
+//        int rollNumber = input.nextInt();
+//        System.out.println("Your roll number is "+ rollNumber);
+//        String name = input.next();
+//        System.out.println(name);
+        float marks = input.nextFloat();
+        System.out.println(marks);
+    }
+}
