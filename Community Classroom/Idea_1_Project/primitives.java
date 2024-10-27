32077aa042ba19e16f500df1dca3e08b4b0fd696
Rohit  Maity <116875489+TheHydrator@users.noreply.github.com>
2024-10-27 15:27:56 -0500

    Add files via upload

---
+++ Community Classroom/Idea_1_Project/primitives.java
@@ -0,0 +1,9 @@
+public class primitives {
+    public static void manin(String[] args){
+        int  rollNumber = 63;
+        float marks = 69.69f;
+        double largeDecimalNumbers = 48724874.74247847;
+        boolean check = true;
+        long IntChecker = 786786986986L;
+    }
+}
